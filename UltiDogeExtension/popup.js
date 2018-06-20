dc2baea229b861d66c20c298fc9ea8998a3697b8
USCORP\ChunK <fai_kwok@ultimatesoftware.com>
2018-06-19 20:53:40 -0400

    Add Show Page Perks button

--- UltiDogeExtension/popup.js
+++ UltiDogeExtension/popup.js
@@ -39,6 +39,62 @@ function loginWithUltiPro()
 		   });	
 	
 }
+
+function refreshDeals()
+{
+	chrome.tabs.getSelected(null, function(tab) {
+        var tabUrl = tab.url;
+        var actionurl="http://localhost:10829/home/HasDealsInSite?url="+tabUrl+"&userId="+localStorage["UltiProUserId"]+"&giftCardOption="+localStorage["UltiProGiftCardOption"]+"&charityOption="+localStorage["UltiProCharityOption"]+"&dealsOption="+localStorage["UltiProDealOption"];
+
+	   $.ajax(
+		   {
+			   url: actionurl,
+			   contentType: "application/json;",
+			   dataType: "json",
+			   type: "GET",
+			   success: function (result) {
+				   var foundPerk=0;
+				   for (var i = 0; i < result.length; i++) {
+				   		if (result[i]["TypeOfDeal"] != '') {
+							iconUrl = '';
+							localStorage["UltiWebsitePerk"]=result[i]["OnClickUrl"];
+							foundPerk++;
+					   		
+					   		if (result[i]["TypeOfDeal"] == "Discount") {
+					   			iconUrl = 'https://pics.me.me/fat-doge-8386016.png';
+					   		}
+					   		else if (result[i]["TypeOfDeal"] == "Gift Card") {
+					   			iconUrl = 'https://apprecs.org/ios/images/app-icons/256/4e/851878478.jpg';
+					   		}
+					   		else {
+					   			iconUrl = 'https://shibe.digital/wishing_well/assets/og_doge.png';
+					   		}
+
+							redirectUrl = result[i]["OnClickUrl"];
+							chrome.notifications.create(
+								'name-for-notification' + i,{   
+								type: 'basic', 
+								iconUrl: iconUrl, 
+								title: result[i]["TypeOfDeal"]+"..Woof Woof", 
+								message: result[i]["Message"]
+								},
+							function() {} 
+							);						
+				   		}
+				   }
+			   }
+		   });	
+    });
+}
+
+chrome.notifications.onClicked.addListener(function(notificationId)
+	{
+		chrome.notifications.clear(notificationId);
+		chrome.tabs.create({url: redirectUrl},function(){})
+	}
+);
+
+
 function ResetLogin()
 {
 	//alert("Resetting login");
@@ -75,6 +131,7 @@ $(document).ready(function(){
 
 	var oauth_token = localStorage.getItem("UltiProUserId");
 	document.getElementById("ultiProloginButton").addEventListener("click", loginWithUltiPro);
+	document.getElementById("refreshDealsButton").addEventListener("click", refreshDeals);
 	document.getElementById("ultiPrologoutButton").addEventListener("click", logout);
 	document.getElementById("DealRange").addEventListener("change", DealChanged);
 	document.getElementById("GiftCardRange").addEventListener("change", GiftCardChanged);
